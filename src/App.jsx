90c851f3e9f53937462608d0462cb852504de347
oumaymaboujeh <omaymaboujeh@gmail.com>
2024-02-27 14:24:49 +0100

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import * as React from "react";
 
 function App() {
-
   return (
-    
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    
+  <div>
+  <h1>My Hacker Stories</h1>
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" />
+  <hr />
+  <List />
+  </div>
   );
-}
+  }
+  
+function List() {
+  return (
+  <ul>
+  {list.map(function (item) {
+  return (
+  <li key={item.objectID}>
+  <span>
+  <a href={item.url}>{item.title}</a>
+  </span>
+  <span>{item.author}</span>
+  <span>{item.num_comments}</span>
+  <span>{item.points}</span>
+  </li>
+  );
+  })}
+  </ul>
+  );
+  }
 
-export default App;
+  function Search() {
+    return (
+    <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+    </div>
+    );
+    }
